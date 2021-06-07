621327908dc38e54e7f7a94463ea97444645eb45
Hamza Saif <hamzasaiff@gmail.com>
2021-06-08 00:37:30 +0500

    Added Slider api with CRUD

---
+++ routes/sliderRoutes.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const sliderController = require('../controller/sliderController');
+
+const router = express.Router();
+
+// Creating Slider
+/**
+ *@swagger
+ *  /v1/slider:
+ *  post:
+ *    tags:
+ *    - [Slider API's]
+ *    summary: "Use to Create Slider"
+ *    description: "This API used for creating Slider"
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "Data for Creating Slider"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *              backgroundImage:
+ *                  type: "string"
+ *              title:
+ *                  type: "string"
+ *              items:
+ *                  type: "string"
+ *              buttonLabel:
+ *                  type: "string"
+ *              buttonLink:
+ *                  type: "string"
+ *    responses:
+ *      "400":
+ *        description: "Invalid input"
+ *      "201":
+ *        description: "Slider Created Successfully"
+ */
+
+// Get one Slider by ID
+/**
+ *@swagger
+ *  /v1/slider/60be7439f8e5642f8c8fb398:
+ *  get:
+ *    tags:
+ *    - [Slider API's]
+ *    summary: "Use to find one Slider"
+ *    description: "This API used to find single/one Slider"
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    responses:
+ *      "400":
+ *        description: "Invalid input"
+ *      "201":
+ *        description: "Show Slider details of specific ID"
+ */
+
+// Get All Sliders
+/**
+ *@swagger
+ *  /v1/slider:
+ *  get:
+ *    tags:
+ *    - [Slider API's]
+ *    summary: "Use to find All Sliders"
+ *    description: "This API used to find All Sliders"
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    responses:
+ *      "400":
+ *        description: "Invalid input"
+ *      "201":
+ *        description: "Show All the Sliders"
+ */
+
+// Udpate/Patch Slider
+/**
+ *@swagger
+ *  /v1/slider/60be7439f8e5642f8c8fb398:
+ *  patch:
+ *    tags:
+ *    - [Slider API's]
+ *    summary: "Use to Update Slider"
+ *    description: "This API used for Updating Slider"
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "Data for Updating Slider"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *              backgroundImage:
+ *                  type: "string"
+ *              title:
+ *                  type: "string"
+ *              items:
+ *                  type: "string"
+ *              buttonLabel:
+ *                  type: "string"
+ *              buttonLink:
+ *                  type: "string"
+ *    responses:
+ *      "400":
+ *        description: "Invalid input"
+ *      "201":
+ *        description: "Slider Updated Successfully"
+ */
+
+// Delete Slider by ID
+/**
+ *@swagger
+ *  /v1/slider/:id:
+ *  delete:
+ *    tags:
+ *    - [Slider API's]
+ *    summary: "Use to Delete Slider by ID"
+ *    description: "This API used to Delete Slider by ID"
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - in: "body"
+ *      name: "body"
+ *      description: "Data for Updating Slider"
+ *      required: true
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *              id:
+ *                  type: "string"
+ *    responses:
+ *      "400":
+ *        description: "Invalid input"
+ *      "201":
+ *        description: "Slider deleted Successfully"
+ */
+
+router
+  .route('/')
+  .get(sliderController.getAllSliders)
+  .post(sliderController.createSlider);
+
+router
+  .route('/:id')
+  .get(sliderController.getSlider)
+  .patch(sliderController.updateSlider)
+  .delete(sliderController.deleteSlider);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,12 +10,16 @@ const AppError = require('./utils/AppError');
 const userRoute = require('./constants/appConstants').routeConsts.userRoute; // User Api constant
 const quoteRoute = require('./constants/appConstants').routeConsts.quoteRoute; // Quote Api constant
 const emailRoute = require('./constants/appConstants').routeConsts.emailRoute; // for emails handling / subscription
-const swaggerRoute = require('./constants/appConstants').routeConsts.swaggerDocRoute; // swagger doc constant
+const swaggerRoute = require('./constants/appConstants').routeConsts
+  .swaggerDocRoute; // swagger doc constant
 const userRouter = require('./routes/userRoutes'); // userRoute
-const quoteRouter = require('./routes/quoteRoutes');// quote Route
-const emailRouter = require('./routes/emailRoutes');// email route
+const quoteRouter = require('./routes/quoteRoutes'); // quote Route
+const emailRouter = require('./routes/emailRoutes'); // email route
 const globalErrorHandler = require('./utils/errorHandler'); // errorHandler
 const swaggerOptions = require('./constants/swaggerOptions');
+
+const sliderRoute = require('./constants/appConstants').routeConsts.sliderRoute;
+const sliderRouter = require('./routes/sliderRoutes');
 
 const PORT = process.env.PORT || 3000; // port
 const app = express();
@@ -37,13 +41,15 @@ app.use(swaggerRoute, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(userRoute, userRouter); // user route
 app.use(quoteRoute, quoteRouter); // quote route
 app.use(emailRoute, emailRouter); // email route
+app.use(sliderRoute, sliderRouter); // slider route
+
 app.all('*', (req, res, next) => {
-	next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
 //error handller
 app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
-	console.log(`Listening on Port ${PORT}`);
+  console.log(`Listening on Port ${PORT}`);
 });
