caf96b0044021f8b42837b44eded9cfc8b8937f5
Azeem-Haider <azeemhaider0322@gmail.com>
2021-06-10 18:35:27 +0500

    bug fix

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const AppError = require('./utils/AppError');
 const userRoute = require('./constants/appConstants').routeConsts.userRoute; // User Api constant
 const quoteRoute = require('./constants/appConstants').routeConsts.quoteRoute; // Quote Api constant
 const emailRoute = require('./constants/appConstants').routeConsts.emailRoute; // for emails handling / subscription
-const reviewRoute=require('./constants/appConstants').routeConsts.reviewRoute; // for reviews
+//const reviewRoute=require('./constants/appConstants').routeConsts.reviewRoute; // for reviews
 const swaggerRoute = require('./constants/appConstants').routeConsts.swaggerDocRoute; // swagger doc constant
 const userRouter = require('./routes/userRoutes'); // userRoute
 const quoteRouter = require('./routes/quoteRoutes');// quote Route
 const emailRouter = require('./routes/emailRoutes');// email route
-const reviewRouter= require('./routes/reviewRoutes');// review Route
+//const reviewRouter= require('./routes/reviewRoutes');// review Route
 const globalErrorHandler = require('./utils/errorHandler'); // errorHandler
 const swaggerOptions = require('./constants/swaggerOptions');
 
