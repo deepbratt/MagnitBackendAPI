8776de0713881fc549b6eca0c10068683eebcb53
Azeem Haider <68206777+Azeem-Haider@users.noreply.github.com>
2021-06-14 23:31:16 +0500

    Update servicesController.js

--- controller/servicesController.js
+++ controller/servicesController.js
@@ -1,6 +1,6 @@
 const Services = require('../model/servicesModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const AppError = require('../utils/AppError');
 const { uploadFile } = require('../utils/s3');
 
 exports.createService = catchAsync(async (req, res, next) => {
