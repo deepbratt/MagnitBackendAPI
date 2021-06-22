61c0fb05c68d2d4ffd09a26ed7c4b0b6fb644f10
Azeem-Haider <azeemhaider0322@gmail.com>
2021-06-22 17:12:45 +0500

    added page Api/updated

--- model/blogsModel.js
+++ model/blogsModel.js
@@ -1,31 +1,31 @@
 const mongoose = require('mongoose');
 
 const blogsSchema = new mongoose.Schema({
-  image: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: [true, 'Blog Must have a Title'],
-    unique: [true, 'A blog Title must be unique'],
-  },
-  text: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  buttonLabel: {
-    type: String,
-  },
-  views: {
-    type: Number,
-    default: 0,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+	image: {
+		type: String,
+	},
+	title: {
+		type: String,
+		required: [true, 'Blog Must have a Title'],
+		unique: [true, 'A blog Title must be unique'],
+	},
+	text: {
+		type: String,
+	},
+	link: {
+		type: String,
+	},
+	buttonLabel: {
+		type: String,
+	},
+	views: {
+		type: Number,
+		default: 0,
+	},
+	date: {
+		type: Date,
+		default: Date.now(),
+	},
 });
 
 const Blogs = mongoose.model('Blogs', blogsSchema);
