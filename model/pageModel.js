60e5674616f8b6e4cbb6cc3f8239e4758583bd69
Hamza Saif <hamzasaiff@gmail.com>
2021-06-30 22:36:08 +0500

    commit

--- model/pageModel.js
+++ model/pageModel.js
@@ -4,104 +4,104 @@ const validator = require('validator');
 const { appErrors } = require('../constants/appConstants');
 
 const sectionObj = {
-	queryParams: Object,
-	order: Number,
-	title: String,
-	subTitle: String,
-	dataArray: { type: Array, default: undefined },
+  queryParams: Object,
+  order: Number,
+  title: String,
+  subTitle: String,
+  dataArray: { type: Array, default: undefined },
 };
 
 const pageSchema = new mongoose.Schema({
-	metaData: {
-		title: {
-			type: String,
-			required: [true, 'title is required'],
-		},
-		canonical: {
-			type: String,
-			unique: true,
-			required: [true, `Canonial is required`],
-		},
-		description: {
-			type: String,
-			required: [true, 'description is required'],
-		},
-		keywords: {
-			type: String,
-			required: [true, 'keywords are required'],
-		},
-	},
-	sections: {
-		banner: {
-			...sectionObj,
-		},
-		services: {
-			...sectionObj,
-		},
-		reviews: {
-			...sectionObj,
-		},
-		homeSlider: {
-			...sectionObj,
-		},
-		awards: {
-			...sectionObj,
-		},
-		company: {
-			...sectionObj,
-		},
-		opportunites: {
-			...sectionObj,
-		},
-		trainingCertification: {
-			...sectionObj,
-		},
-		workFlow: {
-			...sectionObj,
-		},
-		joinTeam: {
-			...sectionObj,
-		},
-		jobBenifits: {
-			...sectionObj,
-		},
-		FAQs: {
-			...sectionObj,
-		},
-		appAdminPanel: {
-			...sectionObj,
-		},
-		appSolutions: {
-			...sectionObj,
-		},
-		blogs: {
-			...sectionObj,
-		},
-		benefits: {
-			...sectionObj,
-		},
-		hiringOptions: {
-			...sectionObj,
-		},
-		factsAboutUs: {
-			...sectionObj,
-		},
-		ourWork: {
-			...sectionObj,
-		},
-		ourObjectives: {
-			...sectionObj,
-		},
-		howitWorks: {
-			...sectionObj,
-		},
-		caseStudies: {
-			...sectionObj,
-		},
-		ourSolutions: {
-			...sectionObj,
-		},
-	},
+  metaData: {
+    title: {
+      type: String,
+      required: [true, 'title is required'],
+    },
+    canonical: {
+      type: String,
+      unique: true,
+      required: [true, `Canonial is required`],
+    },
+    description: {
+      type: String,
+      required: [true, 'description is required'],
+    },
+    keywords: {
+      type: String,
+      required: [true, 'keywords are required'],
+    },
+  },
+  sections: {
+    banner: {
+      ...sectionObj,
+    },
+    services: {
+      ...sectionObj,
+    },
+    reviews: {
+      ...sectionObj,
+    },
+    homeSlider: {
+      ...sectionObj,
+    },
+    awards: {
+      ...sectionObj,
+    },
+    company: {
+      ...sectionObj,
+    },
+    opportunites: {
+      ...sectionObj,
+    },
+    trainingCertification: {
+      ...sectionObj,
+    },
+    workFlow: {
+      ...sectionObj,
+    },
+    joinTeam: {
+      ...sectionObj,
+    },
+    jobBenifits: {
+      ...sectionObj,
+    },
+    FAQs: {
+      ...sectionObj,
+    },
+    appAdminPanel: {
+      ...sectionObj,
+    },
+    appSolutions: {
+      ...sectionObj,
+    },
+    blogs: {
+      ...sectionObj,
+    },
+    benefits: {
+      ...sectionObj,
+    },
+    hiringOptions: {
+      ...sectionObj,
+    },
+    factsAboutUs: {
+      ...sectionObj,
+    },
+    ourWork: {
+      ...sectionObj,
+    },
+    ourObjectives: {
+      ...sectionObj,
+    },
+    howitWorks: {
+      ...sectionObj,
+    },
+    caseStudies: {
+      ...sectionObj,
+    },
+    ourSolutions: {
+      ...sectionObj,
+    },
+  },
 });
 
 pageSchema.index({ 'metaData.canonical': 1 });
